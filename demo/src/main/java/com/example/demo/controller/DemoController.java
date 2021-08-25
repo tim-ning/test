814b7b820e23c5699ac98ffb9f1a7db153dcdc2e
Tim <tim@example.com>
2021-08-24 21:33:53 -0500

    2

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
 
 @RestController
 public class DemoController {
 
 	@RequestMapping("/test")
-	public String readTest() {
-		return "<h1>Hello world!!</h1>";
+	public List<Book> readTest() {
+		List<Book> list = new ArrayList<>();
+		list.add(new Book(1, "Hello world!", "JK . Rowling"));
+		list.add(new Book(2, "Lord of The Rings", "Tolkien"));
+		return list;
 	}
 }
