c4d76d2c1711bba7d569b290824557ce29048250
Tim <tim@example.com>
2021-08-24 21:48:06 -0500

    3

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Book;
 
 @RestController
+@CrossOrigin
 public class DemoController {
 
 	@RequestMapping("/test")
